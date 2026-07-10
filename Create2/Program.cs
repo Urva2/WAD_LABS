30fea2ddf8b57e66512d89b29d229aa5937c1c68
unknown <urvapatel6674@gmail.com>
2026-07-10 16:53:55 +0530

    Lab1 Completed.

---
+++ Create2/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("1. Addition (+)");
+Console.WriteLine("2. Subtraction (-)");
+Console.WriteLine("3. Multiplication (*)");
+Console.WriteLine("4. Division (/)");
+Console.WriteLine("5. Modulus (%)");
+Console.WriteLine("6. Exit");
+Console.Write("\nSelect an option (1-6): ");
+
+int choice=int.Parse(Console.ReadLine());
+if (choice == 6)
+{
+    Console.WriteLine("Thank you for using Calculator.Good Bye!!");
+    return;
+}
+
+while(choice < 1 || choice > 6)
+{
+    Console.WriteLine("Please Enter Valide Choice:");
+    choice=int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Enter Number1:");
+double num1=double.Parse(Console.ReadLine());
+Console.WriteLine("Enter Number2:");
+double num2 = double.Parse(Console.ReadLine());
+switch (choice)
+{
+    case 1:
+        Console.WriteLine($"Result: {num1} + {num2} = {num1 + num2}");
+        break;
+
+    case 2:
+        Console.WriteLine($"Result: {num1} - {num2} = {num1 - num2}");
+        break;
+
+    case 3:
+        Console.WriteLine($"Result: {num1} * {num2} = {num1 * num2}");
+        break;
+
+    case 4:
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: Division by zero is mathematically undefined.");
+        }
+        else
+        {
+            Console.WriteLine($"Result: {num1} / {num2} = {num1 / num2}");
+        }
+        break;
+    case 5:
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: Modulus by zero is not allowed.");
+        }
+        else
+        {
+            Console.WriteLine($"Result: {num1} % {num2} = {num1 % num2}");
+        }
+        break;
+
+}
---
+++ Develop/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Enter All Subject Marks Out Of 100.");
+Console.WriteLine("Enter Sub1 Marks:");
+int sub1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Sub2 Marks:");
+int sub2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Sub3 Marks:");
+int sub3 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Sub4 Marks:");
+int sub4 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Sub5 Marks:");
+int sub5 = int.Parse(Console.ReadLine());
+
+int total = sub1 + sub2 + sub3 + sub4 + sub5;
+double percentage = (total * 100) / 500;
+string grade;
+if (percentage >= 80 && percentage <= 100)
+{
+    grade = "A";
+}
+else if (percentage >= 60 && percentage <= 79)
+{
+    grade = "B";
+}
+else if (percentage >= 35 && percentage <= 59)
+{
+    grade = "C";
+}
+else
+{
+    grade = "Fail";
+}
+Console.WriteLine("Total Marks:" + total + "/500");
+Console.WriteLine("Percentage:" + percentage);
+Console.WriteLine("Grade:" + grade);
